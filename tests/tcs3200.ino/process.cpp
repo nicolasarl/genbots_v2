cefc076b31e8173a925ccd150233d37e81e23fdf
nicolasarl <32613421+nicolasarl@users.noreply.github.com>
2018-02-20 20:01:45 +0000

    Add files via upload

---
+++ tests/tcs3200.ino/process.cpp
@@ -0,0 +1,14 @@
+#ifndef _PROCESS_CPP
+#define _PROCESS_CPP
+#include <Arduino.h>
+#include <math.h>
+#include "subprocess.h"
+#include "pines.h"
+
+void lectura(float input_neuron [ ]){
+
+  tcs(input_neuron);
+
+  
+}
+#endif
---
+++ tests/tcs3200.ino/process.h
@@ -0,0 +1,5 @@
+#ifndef _PROCESS_H
+#define _PROCESS_H
+void lectura(float input_neuron[]);
+
+#endif

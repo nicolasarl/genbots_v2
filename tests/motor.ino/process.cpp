9107e4965b35af4e998a163ca5606cd45bae4802
nicolasarl <32613421+nicolasarl@users.noreply.github.com>
2018-02-20 20:00:41 +0000

    Add files via upload

---
+++ tests/motor.ino/process.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include "pines.h"
+
+int temp2 = 0;
+
+void salida(float output_neuron[]){
+  //Salida motor der
+
+  if (output_neuron[0] < 0){
+    
+    temp2 = int(output_neuron[0]*255*(-1));
+    analogWrite(PWM1, temp2);
+    digitalWrite(SM1, LOW);//hacia detrás
+    digitalWrite(SM1_2, HIGH);
+  }
+  else{
+    temp2 = int(output_neuron[0]*255);
+    analogWrite(PWM1, temp2);
+    digitalWrite(SM1, HIGH);//hacia delante
+    digitalWrite(SM1_2, LOW);    
+  }
+   //Salida motor izq
+
+  if (output_neuron[1] < 0){
+    
+    temp2 = int(output_neuron[1]*255*(-1));
+    analogWrite(PWM2, temp2);
+    digitalWrite(SM2, LOW);//hacia detrás
+    digitalWrite(SM2_2, HIGH);
+  }
+  else{
+    temp2 = int(output_neuron[1]*255);
+    analogWrite(PWM2, temp2);
+    digitalWrite(SM2, HIGH);//hacia delante
+    digitalWrite(SM2_2, LOW);    
+  }
+}
---
+++ tests/motor.ino/process.h
@@ -0,0 +1,4 @@
+#ifndef _PROCESS_H
+#define _PROCESS_H
+void salida(float output_neuron[]);
+#endif
